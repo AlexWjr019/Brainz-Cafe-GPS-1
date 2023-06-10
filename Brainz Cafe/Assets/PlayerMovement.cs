6c2155ffb436a11c019ab98ab70c223576083f64
LilFeeder <0135023@student.uow.edu.my>
2023-06-10 21:14:48 +0800

    sprint & stamina bar

--- Brainz Cafe/Assets/PlayerMovement.cs
+++ Brainz Cafe/Assets/PlayerMovement.cs
@@ -1,24 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    [HideInInspector] public StaminaController _staminaController;
 
-    public Rigidbody2D rb;
+    Rigidbody2D rb;
+    public static bool isSprinting;
 
-    Vector2 movement;
+    float walkSpeed = 5f;
+    float runSpeed = 20f;
+    float speedLimiter = 0.8f;
+    float inputHorizontal;
+    float inputVertical;
+
+
+    private void Start()
+    {
+        _staminaController = GetComponent<StaminaController>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    public void SetRunSpeed(float speed)
+    {
+        runSpeed = speed;
+    }
+
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+
+        if (PlayerMovement.isSprinting)
+        {
+            walkSpeed = 20f;
+        }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-    }
+        if (inputHorizontal != 0 || inputVertical != 0)
+        {
+            if (inputHorizontal != 0 && inputVertical != 0)
+            {
+                inputHorizontal *= speedLimiter;
+                inputVertical *= speedLimiter;
+            }
 
+            rb.velocity = new Vector2(inputHorizontal * walkSpeed, inputVertical * walkSpeed);
+
+            if (!Input.GetKey(KeyCode.LeftShift))
+            {
+                _staminaController.weAreSprinting = false;
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (_staminaController.playerStamina > 0)
+                {
+                    rb.velocity = new Vector2(inputHorizontal * runSpeed, inputVertical * runSpeed);
+                    _staminaController.weAreSprinting = true;
+                    _staminaController.Sprinting();
+                }
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(0f, 0f);
+        }
+    }
 }
