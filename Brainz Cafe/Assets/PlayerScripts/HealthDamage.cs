d82ea1281923c65e67dab25179b80c107bf33898
Phung Yung Ren <0133205@kdu-online.com>
2023-06-25 00:25:02 +0800

    Added YungRen Part
    Added health bar and satisfaction mode

---
+++ Brainz Cafe/Assets/PlayerScripts/HealthDamage.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthDamage : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.V))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damege)
+    {
+        currentHealth -= damege;
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+}
